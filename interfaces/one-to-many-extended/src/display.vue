3ddcef6d0008573a421a795c8dde5f3b7b877406
Philleep Florence <email@philleep.com>
2020-10-21 18:32:50 -0400

    0.3.5: New Interface - O2M - Renders a One to Many Interface that allows for Filtering by Collection and more

---
+++ interfaces/one-to-many-extended/src/display.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="no-wrap many-to-many-extended">{{ itemCount }}</div>
+</template>
+
+<script>
+	import mixin from '@directus/extension-toolkit/mixins/interface';
+	export default {
+		name: 'ReadonlyOneToManyExtended',
+		mixins: [mixin],
+		computed: {
+			itemCount() {
+				const render = this.$helpers.micromustache.compile(this.options.item_count);
+				
+				return render({
+					count: (this.value || []).length
+				});
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.many-to-many-extended {
+		position: relative;
+	}
+</style>
